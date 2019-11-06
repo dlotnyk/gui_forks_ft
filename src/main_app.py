6128147fe665a8b15364e28c0f84f3e7580f0058
dlvision <dmytro.lotnyk@gmail.com>
2019-11-06 22:31:10 +0100

    figures added and numpy

--- src/main_app.py
+++ src/main_app.py
@@ -1,6 +1,11 @@
 import tkinter
 from tkinter import ttk
 from typing import Set, Dict, Tuple, List
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+import numpy as np
 
 
 class ForksGUI:
@@ -13,17 +18,30 @@ class ForksGUI:
         self.label = tkinter.Label(master, text="This is our first GUI!")
         self.label.pack()
 
-        self.greet_button = tkinter.Button(master, text="Greet", command=self.greet)
-        self.greet_button.pack()
+        self.greet_button = tkinter.Button(master, text="Greet_DIma", command=self.greet)
+        self.greet_button.pack(side=tkinter.BOTTOM)
 
         self.close_button = tkinter.Button(master, text="Close", command=master.quit)
-        self.close_button.pack()
+        self.close_button.pack(side=tkinter.BOTTOM)
 
     def greet(self) -> None:
         print("HIIIIIIIIIIII!!!!")
+
+    def figure_1(self) -> None:
+        """
+        figure in matplotlib
+        :return:
+        """
+        fig = Figure(figsize=(5, 4), dpi=100)
+        t = np.arange(0, 3, .01)
+        fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
+        canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
 
 if __name__ == "__main__":
     root = tkinter.Tk()
     my_gui = ForksGUI(root)
+    my_gui.figure_1()
     root.mainloop()
